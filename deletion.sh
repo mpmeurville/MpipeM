ddb90da4270839e35be5a78ff014a9705fb42873
mmeurvil <marie-pierre.meurville@unil.ch>
2017-12-07 15:01:07 +0000

    clean comments

--- deletion.sh
+++ deletion.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 cat $1 | while read -r line; do 
-#new= $line | tr -d '\r'
-#echo $new
-#echo "/archive/cig/fankhauser/plantgrowth/Lemnatec/Databases/$line"
+
 cp -R "/archive/cig/fankhauser/plantgrowth/Lemnatec/Databases/$line" ./Backups
 
 done
